a4ed83d4d5170f9966ec89aafbaf0713f0a16d98
eyetech <eyetech@eyetech.home>
2019-12-19 06:59:24 +0200

    efood initial state

---
+++ public/index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans|Playfair+Display&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="/stylesheets/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>E-food</title>
+</head>
+
+<body>
+
+
+    <!-- Carousel -->
+    <section id="showcase">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-t">
+                <a class="navbar-brand" href="#">E-food</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Delivery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">contact</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Reservation</button>
+                    </form>
+                </div>
+            </nav>
+        </header>
+        <div class="h-text">
+            <div class="wrap-content text-light">
+                <h1 id="display">E-FOOD</h1>
+                <h2>BEST RESTAURANT</h2>
+                <p>Hygienic Food - Special dish - delicious specialities - Nutrious&Tasty - delicious
+                    Special dish - delicious specialities - Nutrious&Tasty - delicious
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_1 container">
+
+            <div class="mycol_1">
+                <img class="section_1_img" src="images/image_4.jpg" alt="">
+            </div>
+
+            <div class="mycol">
+                <div class="section_1_box">
+                    <h4>Something new</h4>
+                    <h2>AN EXTRAODINARY EXPERIENCE</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur
+                        adipisicing elit. Porro repudiandae necessitatibus
+                        libero odit sequi. Vitae cumque facere minus magnam
+                        dolores quasi placeat vero, laborum quidem
+                        distinctio ducimus eligendi nostrum. Ea!</p>
+                </div>
+            </div>
+
+            <div class="mycol_2">
+                 <img class="section_1_img section_1_img_1" src="images/image_1.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <!-- Section 3-->
+
+    <section class="section_3 container-fluid">
+        
+        <div class="section_3_image">
+            <p class="section_3_text">Food for the soul</p>
+        </div>
+    </section>
+
+    <section class="section_4 container">
+        <div class="first_box">
+            <div class="content">
+                <p class="subtitle">Something New</h3>
+                <p class="title">OUR SIGNATURE</h1>
+                <p class="lead">Pork Tenderloin in green pepper ....................... <span>$20</span></p>
+                <small>Pork/Tenderloin/Green Pepper/Veggies</small><br /><br />
+                <input type="submit" class="my-btn" value="Order Now">
+           </div>
+        </div>
+
+        <div class="second_box">
+            <img class="section_4_img" src="images/image_3.jpg" width="100%" alt="">
+        </div>
+    </section>
+
+    <div class="space"></div>
+
+    <section class="section_5">
+        <div class="container">
+            <div class="wrapper">
+                <p>5 Stars</p>
+                <div class="menu_div">
+                    <p>The Menu</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="space"></div>
+
+    <section class="section_6 container">
+
+        <div class="menu_1">
+        <input type="submit" class="my-btn" value=" Starters">
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+        </div>
+
+        <div class="menu_2">
+        <input type="submit" class="my-btn" value=" Main">
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+            <span>Pork Tenderloin in green pepper   ................  $20</span>
+            <small>pork/Tenderloin/Green Pepper</small>
+            <br /><br />
+            <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+            <span>Pork Tenderloin in green pepper   ................  $20</span>
+            <small>pork/Tenderloin/Green Pepper</small>
+            <br /><br />
+            <a href="#" >Order now</a>
+            </div>
+        </div>
+
+        <div class="menu_3">
+        <input type="submit" class="my-btn" value=" Deserts">
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+
+            <div class="content">
+
+                <span>Pork Tenderloin in green pepper   ................  $20</span>
+                <small>pork/Tenderloin/Green Pepper</small>
+                <br /><br />
+                <a href="#" >Order now</a>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="section_7">
+        <div class="back_img container-fluid " >
+            <p class="reservation-title">MAKE A RESERVATION</p>
+            <!--
+            <form action="" class="container">
+                <div class="form-inline">
+                    <div class="form-group my-form">
+                        <div>
+                        <input type="DATE" class="my-form-control" placeholder="Date" />
+                        </div>
+                        <div>
+                        <input type="time" class="my-form-control " placeholder="Time" />
+                        </div>
+                        <div>
+                        <input type="number" class="my-form-control " placeholder="Persons" />
+                        </div>
+                    </div>
+                    
+                </div>
+                <input type="submit" value="Make Reservation" class="my-btn-2" />
+            </form>
+            -->
+
+            <form action="">
+                <div class="resp">
+                    <div class="one">
+                        <input type="DATE" class="my-form-control">
+                    </div>
+
+                    <div class="two">
+                    <input type="TIME" class="my-form-control">
+                    </div>
+
+                    <div class="three">
+                        <input type="number" class="my-form-control" placeholder="Persons">
+                    </div>
+                    <div>
+
+                    </div>
+                    <div class="four">
+                        <input type="submit" class="my-form-control" value="Submit">
+                    </div>
+                    <div></div>
+                </div>
+            </form>
+        </div>
+    </section>
+    <div class="section_8">
+        <div class="footer container">
+            <div class="left">
+                <h1>E-FOOD</h1>
+                <h3>RESTAURANT</h3>
+                <p>Copyright 2019 All rights reserved</p>
+                <p>Designed by Eyetech</p>
+            </div>
+            <div class="middle">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                    rchitecto magni est adipisci culpa, atque placeat vel neque 
+                    reprehenderit ratione fugit iste inve
+                    ntore distinctio libero facere nulla exercitationem 
+                    molestiae corrupti eaque.</p>
+            </div>
+            <div class="right">
+                <p>145, Creside Lane CA<br/>info@efood.com<br/>084 048 2861</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- JS CDNs -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
